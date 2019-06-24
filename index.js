b7c21fa9f54f86b4e94687a1ba713561fe602e68
kellyeryan <kellyeryan@gmail.com>
2019-06-24 18:25:36 -0400

    Finish Beatles Loops Lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+
+function theBeatlesPlay(musicians, instruments){
+  var newArray = [];
+  for(var i = 0; i < musicians.length; i++){
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray
+}
+
+
+function johnLennonFacts(facts) {
+  var i = 0;
+  var newArr = [];
+  while(i < facts.length) {
+    newArr.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return newArr;
+}
+
+function iLoveTheBeatles(num) {
+  var newishArr = [];
+  do{
+    newishArr.push("I love the Beatles!");
+    num++;
+  } while(num < 15);
+  return newishArr;
+}
+
